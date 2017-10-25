c05f510d1f6f11d8552489142e550d4272c020cf
mamonreal <ma.monreal@alumnos.urjc.es>
2017-10-25 12:04:19 +0200

    Cambio LeafIterator

--- src/practica2/LeafIterator.java
+++ src/practica2/LeafIterator.java
@@ -5,6 +5,7 @@
  */
 package practica2;
 
+import java.util.Collections;
 import java.util.Deque;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -20,41 +21,57 @@ import material.tree.Tree;
  */
 public class LeafIterator <T> implements Iterator<Position<T>> {
 	
-	private Deque<Position<T>> nodeQueue = new LinkedList<>();
+	private Deque<Position<T>> nodeStack = new LinkedList<>();
 	private final Tree<T> tree;
 	
     public LeafIterator(Tree<T> tree) {
         this.tree = tree;
-        LinkedList<Position<T>> auxList = new LinkedList<>();
-        auxList.add(tree.root());
-        while(! auxList.isEmpty()) {
-        	Position<T> p = auxList.pollFirst();
-        	if (tree.isLeaf(p)) {
-        		nodeQueue.add(p);
-        	}
-        	else {
-        		/* En el caso de que tenga que iterar los hijos de
-        		 * izquierda a derecha
-        		 */
+        if (tree.isLeaf(tree.root())) {
+        	nodeStack.add(tree.root());
+        }
+        else {
+        	nodeStack.add(tree.root());
+            Position<T> aux = nodeStack.removeFirst();
+            LinkedList<Position<T>> children = new LinkedList<>();
+            
+           while(!tree.isLeaf(aux)) { 
+            	for (Position <T> node: this.tree.children(aux))
+	    			children.addFirst(node);
+	    		while (! children.isEmpty()) {
+	    			nodeStack.addFirst(children.pollFirst());
+	    		}
+	    		aux = nodeStack.removeFirst();
+           }
+        }
+        
+//        while(! auxList.isEmpty()) {
+//        	Position<T> p = auxList.pollFirst();
+//        	if (tree.isLeaf(p)) {
+//        		nodeStack.add(p);
+//        	}
+//        	else {
+//        		/* En el caso de que tenga que iterar los hijos de
+//        		 * izquierda a derecha
+//        		 */
 //        		LinkedList <Position<T>> children = new LinkedList<>();
 //        		for (Position <T> node: this.tree.children(p))
 //        			children.addFirst(node);
 //        		while (! children.isEmpty()) {
 //        			auxList.addFirst(children.pollFirst());
 //        		}
-        		for (Position <T> node: this.tree.children(p))
-        			auxList.addLast(node);
-        	}        		
-        }
+//        		for (Position <T> node: this.tree.children(p))
+//        			auxList.addLast(node);
+//        	}        		
+//        }
     }
             
     @Override
     public boolean hasNext() {
-        return ! nodeQueue.isEmpty();
+        return ! nodeStack.isEmpty();
     }
 
     @Override
     public Position<T> next() {
-    	return nodeQueue.pollFirst();
+    	return nodeStack.pollFirst();
     }
 }
